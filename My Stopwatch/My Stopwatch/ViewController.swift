5a065ac47b421a9cac19d83ed83bdc1bf8efbcd2
dennis910130 <sic046@ucsd.edu>
2015-07-06 10:51:06 -0700

    finished stop watch

--- My Stopwatch/My Stopwatch/ViewController.swift
+++ My Stopwatch/My Stopwatch/ViewController.swift
@@ -10,9 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    var timer = NSTimer()
+    
+    var startTime = NSTimeInterval()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+
+        
+    }
+    
+
+    var baseTime:NSTimeInterval = 0
+    var interval:NSTimeInterval = 0
+    
+    var running = false
+    
+    @IBAction func resetButton(sender: AnyObject) {
+        if (!running) {
+            baseTime = 0
+            timeLabel.text = "00:00:00:000"
+        }
+    }
+    
+    @IBAction func stopButton(sender: AnyObject) {
+        if (running) {
+            timer.invalidate()
+            baseTime += interval
+            running = false
+        }
+    }
+    
+    @IBAction func startButton(sender: AnyObject) {
+        if (!running) {
+            startTime = NSDate.timeIntervalSinceReferenceDate()
+
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: Selector("updateTime"), userInfo: nil, repeats: true)
+            running = true
+        }
+    }
+    func updateTime() {
+        var currentTime = NSDate.timeIntervalSinceReferenceDate()
+        interval = currentTime - startTime
+        var string = stringFromTimeInterval(interval + baseTime)
+        timeLabel.text = string
+        
+    }
+    
+    func stringFromTimeInterval(interval:NSTimeInterval) -> String {
+        var time = interval
+        var ms = Int((time % 1) * 1000)
+        var sec = Int(time % 60)
+        var min = Int((time / 60) % 60)
+        var hour = Int(time / 3600)
+        return String(format: "%0.2d:%0.2d:%0.2d:%0.3d", hour, min, sec, ms)
     }
 
     override func didReceiveMemoryWarning() {
--- navigation_bar/navigation_bar/ViewController.swift
+++ navigation_bar/navigation_bar/ViewController.swift
@@ -12,28 +12,26 @@ class ViewController: UIViewController {
     
     var timer = NSTimer()
     
-    var start:Bool = false
     
     var time:Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("result"), userInfo: nil, repeats: true)
+        
         
     }
     
     @IBAction func resetButton(sender: AnyObject) {
-            time = 0
-            timeLabel.text = "0"
+        time = 0
+        timeLabel.text = "0"
 
     }
     @IBAction func stopButton(sender: AnyObject) {
-        start = false
     }
     
     @IBAction func startButton(sender: AnyObject) {
-        start = true
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("result"), userInfo: nil, repeats: true)
     }
     
     @IBOutlet weak var timeLabel: UILabel!
