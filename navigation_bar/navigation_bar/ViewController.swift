c1e99e9879e6b2f6964d6a819ec8cd8ed86b0e65
dennis910130 <sic046@ucsd.edu>
2015-07-05 23:25:28 -0700

    come back

--- navigation_bar/navigation_bar/ViewController.swift
+++ navigation_bar/navigation_bar/ViewController.swift
@@ -9,10 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var timer = NSTimer()
+    
+    var start:Bool = false
+    
+    var time:Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("result"), userInfo: nil, repeats: true)
+        
+    }
+    
+    @IBAction func resetButton(sender: AnyObject) {
+            time = 0
+            timeLabel.text = "0"
+
+    }
+    @IBAction func stopButton(sender: AnyObject) {
+        start = false
+    }
+    
+    @IBAction func startButton(sender: AnyObject) {
+        start = true
+    }
+    
+    @IBOutlet weak var timeLabel: UILabel!
+
+    
+    func result() {
+        
+            time += 1
+            timeLabel.text = String(time)
+        
+
     }
 
     override func didReceiveMemoryWarning() {
