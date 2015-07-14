02e8826c823c82a03caae85e889f07d93a6c0677
Si Chen <sic046@ucsd.edu>
2015-07-13 20:25:46 -0700

    add sound shaker

--- Sound Shaker/Sound Shaker/ViewController.swift
+++ Sound Shaker/Sound Shaker/ViewController.swift
@@ -8,10 +8,10 @@
 
 import UIKit
 import AVFoundation
-
+import MapKit
 
 class ViewController: UIViewController {
-
+    var paths = [String]()
     var player = AVAudioPlayer()
     
     override func viewDidLoad() {
@@ -19,11 +19,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         let filemanager:NSFileManager = NSFileManager()
         let files = filemanager.enumeratorAtPath("/Users/sichen/Documents/github/ios_apps/Sound Shaker/")
-        while let file = files?.nextObject() {
-            println(file)
+        while let file = files?.nextObject() as? String {
+            if file.lastPathComponent.pathExtension == "mp3" {
+                paths.append(file.lastPathComponent.stringByDeletingPathExtension)
+            }
         }
+        var annotationPoint = MKAnnotation
         
-        
+    }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent) {
+        if event.subtype == UIEventSubtype.MotionShake {
+            let num = Int(arc4random_uniform(UInt32(paths.count)))
+            var path = NSBundle.mainBundle().pathForResource(paths[num], ofType: "mp3")
+            var error:NSError? = nil
+            player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: path!), error: &error)
+            player.play()
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
