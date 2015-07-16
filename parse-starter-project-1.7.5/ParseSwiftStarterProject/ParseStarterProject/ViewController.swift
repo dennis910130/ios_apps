7c03795ab7b088e0433005316bba2f629a9f19e4
Si Chen <sic046@ucsd.edu>
2015-07-15 19:13:59 -0700

    finish log in form

--- parse-starter-project-1.7.5/ParseSwiftStarterProject/ParseStarterProject/ViewController.swift
+++ parse-starter-project-1.7.5/ParseSwiftStarterProject/ParseStarterProject/ViewController.swift
@@ -10,7 +10,7 @@ import Parse
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
-    
+    var signUpMode = true
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -47,6 +47,8 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         
         
     }
+    
+    /*
     @IBAction func restore(sender: AnyObject) {
         
         activityIndicator.stopAnimating()
@@ -89,7 +91,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     }
     
     
-    @IBAction func importButton(sender: AnyObject) {
+        @IBAction func importButton(sender: AnyObject) {
         
         var image = UIImagePickerController()
         image.delegate = self
@@ -100,7 +102,103 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         
     }
 
+
     @IBOutlet var importedImage: UIImageView!
+    */
+    @IBOutlet var usernameInput: UITextField!
+    
+    func showAlert(title:String, message:String) {
+        let alert:UIAlertController = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    @IBOutlet var topButton: UIButton!
+
+    @IBOutlet var leftLabel: UILabel!
+    @IBOutlet var bottomButton: UIButton!
+    @IBAction func signUp(sender: AnyObject) {
+        
+        self.activityIndicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        self.activityIndicator.center = self.view.center
+        self.activityIndicator.hidesWhenStopped = true
+        self.view.addSubview(self.activityIndicator)
+        self.activityIndicator.startAnimating()
+        UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+        
+        if signUpMode {
+        
+            var user = PFUser()
+            user.password = passwordInput.text
+            user.username = usernameInput.text
+            user.signUpInBackgroundWithBlock({ (success, error) -> Void in
+                if let error = error {
+                    if let errorString = error.userInfo?["error"] as? String {
+                        self.showAlert("Error in SignUp", message: errorString)
+                    } else {
+                        self.showAlert("Error in SignUp", message: "Please try again!")
+                    }
+                    
+                    
+                } else {
+                    
+                    println("successfully signed up")
+                    
+                
+                
+                }
+                UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                self.activityIndicator.stopAnimating()
+            })
+        } else {
+            PFUser.logInWithUsernameInBackground(usernameInput.text, password: passwordInput.text) {
+                (user, error) in
+                if user != nil {
+                    println("Welcome back \(user!.username!)")
+                } else {
+                    if let errorString = error?.userInfo?["error"] as? String {
+                        self.showAlert("Error In LogIn", message: errorString)
+                    } else {
+                        self.showAlert("Error In LogIn", message: "Please try again!")
+                    }
+                }
+                UIApplication.sharedApplication().endIgnoringInteractionEvents()
+                self.activityIndicator.stopAnimating()
+            }
+
+        }
+        
+    }
+    
+    @IBAction func logIn(sender: AnyObject) {
+        self.activityIndicator = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        self.activityIndicator.center = self.view.center
+        self.activityIndicator.hidesWhenStopped = true
+        self.view.addSubview(self.activityIndicator)
+        self.activityIndicator.startAnimating()
+        UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+        
+        if signUpMode == true {
+            topButton.setTitle("Log In", forState: UIControlState.Normal)
+            bottomButton.setTitle("Sign Up", forState: UIControlState.Normal)
+            leftLabel.text = "Not Registered?"
+            signUpMode = false
+        } else {
+            topButton.setTitle("Sign Up", forState: UIControlState.Normal)
+            bottomButton.setTitle("Log In", forState: UIControlState.Normal)
+            leftLabel.text = "Already Registered?"
+            signUpMode = true
+        }
+        UIApplication.sharedApplication().endIgnoringInteractionEvents()
+        self.activityIndicator.stopAnimating()
+        
+        
+    }
+    @IBOutlet var passwordInput: UITextField!
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
